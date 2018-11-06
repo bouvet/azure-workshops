8c09116c4ad60b9794e05394e88d8a2d9b0ccf69
Bjørn Tore Gjerde <bjorntore.gjerde@bouvet.no>
2018-11-06 17:57:50 +0100

    Add missing import

--- Workshop_1/Komplett/AzureWorkshop/AzureWorkshopApp/Controllers/ImagesController.cs
+++ Workshop_1/Komplett/AzureWorkshop/AzureWorkshopApp/Controllers/ImagesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AzureWorkshopApp.Helpers;
+using AzureWorkshopApp.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
