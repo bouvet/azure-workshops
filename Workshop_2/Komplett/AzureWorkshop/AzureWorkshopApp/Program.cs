3d61f6ba8512bf52f1795dc9ac5dda4d06b27a5c
Markus Skallist <markus.skallist@bouvet.no>
2019-01-16 19:04:49 +0100

    application insights

--- Workshop_2/Komplett/AzureWorkshop/AzureWorkshopApp/Program.cs
+++ Workshop_2/Komplett/AzureWorkshop/AzureWorkshopApp/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace AzureWorkshopApp
 {
@@ -14,11 +7,12 @@ namespace AzureWorkshopApp
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights().Build();
     }
 }
