ffe9976089b5dfc7516debb9f46fa5359d670cc5
Haavard Engum <havard.engum@bouvet.no>
2019-01-16 17:05:02 +0100

    La til kodeprosjekt for Workshop2

---
+++ Workshop_2/Komplett/AzureWorkshop/AzureWorkshopApp/Services/StorageService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using AzureWorkshopApp.Helpers;
+using AzureWorkshopApp.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureWorkshopApp.Services
+{
+    public class StorageService : IStorageService
+    {
+        private readonly AzureStorageConfig _storageConfig;
+
+        public StorageService(IOptions<AzureStorageConfig> storageConfig)
+        {
+            _storageConfig = storageConfig != null ? storageConfig.Value : throw new ArgumentNullException(nameof(storageConfig));
+        }
+
+        public AzureStorageConfigValidationResult ValidateConfiguration()
+        {
+            return StorageConfigValidator.Validate(_storageConfig);
+        }
+
+        public async Task<bool> UploadFileToStorage(Stream fileStream, string fileName)
+        {
+            // Create storagecredentials object by reading the values from the configuration (appsettings.json)
+            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+
+            // Create cloudstorage account by passing the storagecredentials
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get reference to the blob container by passing the name by reading the value from the configuration (appsettings.json)
+            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+
+            // Get the reference to the block blob from the container
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            // Upload the file
+            await blockBlob.UploadFromStreamAsync(fileStream);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<List<string>> GetImageUrls()
+        {
+            List<string> imageUrls = new List<string>();
+
+            // Create storagecredentials object by reading the values from the configuration (appsettings.json)
+            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+
+            // Create cloudstorage account by passing the storagecredentials
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            // Create blob client
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get reference to the container
+            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+
+            BlobContinuationToken continuationToken = null;
+
+            //Call ListBlobsSegmentedAsync and enumerate the result segment returned, while the continuation token is non-null.
+            //When the continuation token is null, the last page has been returned and execution can exit the loop.
+            do
+            {
+                //This overload allows control of the page size. You can return all remaining results by passing null for the maxResults parameter,
+                //or by calling a different overload.
+                BlobResultSegment resultSegment = await container.ListBlobsSegmentedAsync("", true, BlobListingDetails.All, 10, continuationToken, null, null);
+
+                foreach (var blobItem in resultSegment.Results)
+                {
+                    imageUrls.Add(blobItem.StorageUri.PrimaryUri.ToString());
+                }
+
+                //Get the continuation token.
+                continuationToken = resultSegment.ContinuationToken;
+            }
+
+            while (continuationToken != null);
+
+            return await Task.FromResult(imageUrls);
+        }
+    }
+}
