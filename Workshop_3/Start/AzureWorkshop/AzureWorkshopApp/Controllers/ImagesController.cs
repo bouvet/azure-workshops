28c4554c573195aacd0e15c4631baf10d53ba68b
pabrah <petterabrahamsen@hotmail.com>
2021-06-09 10:42:50 +0200

    Update ImagesController.cs

--- Workshop_3/Start/AzureWorkshop/AzureWorkshopApp/Controllers/ImagesController.cs
+++ Workshop_3/Start/AzureWorkshop/AzureWorkshopApp/Controllers/ImagesController.cs
@@ -27,7 +27,7 @@ namespace AzureWorkshopApp.Controllers
 
         // TODO: Legg til attributt for at bruker må være innlogget og ha rollen Uploader for å laste oppe bilde.
         // [Authorize(Roles = "Uploader")]
-        // POST /api/images
+        // POST /api/images/upload
         [HttpPost("[action]")]
         public async Task<IActionResult> Upload(ICollection<IFormFile> files)
         {
