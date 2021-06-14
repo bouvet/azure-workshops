9d7d4ba37a3363b1c88a0786f74e0efc5672a6ba
Petter Abrahamsen <petter.abrahamsen2@banenor.no>
2021-06-14 10:14:09 +0200

    Removed usings in ImageService

---
+++ Workshop_4/Komplett/AzureWorkshop/AzureWorkshopApp/Services/IQueueService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureWorkshopApp.Services
+{
+    public interface IQueueService
+    {
+        Task SendQueueMessage(string queueName, string message);
+    }
+}
--- Workshop_4/Komplett/AzureWorkshop/AzureWorkshopFunctionApp/Services/ImageService.cs
+++ Workshop_4/Komplett/AzureWorkshop/AzureWorkshopFunctionApp/Services/ImageService.cs
@@ -1,10 +1,7 @@
 ﻿using AzureWorkshopFunctionApp.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 
 namespace AzureWorkshopFunctionApp.Services
 {
@@ -52,13 +49,13 @@ namespace AzureWorkshopFunctionApp.Services
         }
 
         public Stream GreyScale(Stream image, ImageFormat format)
-        {
+    {
             var bitmap = new Bitmap(image);
             var greyBitmap = new Bitmap(bitmap.Width, bitmap.Height);
 
             ColorMatrix colorMatrix = new ColorMatrix(
                new float[][]
-               {
+        {
                          new float[] {.3f, .3f, .3f, 0, 0},
                          new float[] {.59f, .59f, .59f, 0, 0},
                          new float[] {.11f, .11f, .11f, 0, 0},
