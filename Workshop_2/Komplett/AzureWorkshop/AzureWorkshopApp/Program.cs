8628d6e3c0c81488066369055d683d871a481b0b
Knut Erik Lødding <knuterikl@gmail.com>
2022-09-13 19:59:31 +0200

    oppdatert komplett etter leksjon2

--- Workshop_2/Komplett/AzureWorkshop/AzureWorkshopApp/Program.cs
+++ Workshop_2/Komplett/AzureWorkshop/AzureWorkshopApp/Program.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AzureWorkshopApp
 {
@@ -13,6 +22,15 @@ namespace AzureWorkshopApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    var config = builder.Build();
+                    if (!string.IsNullOrEmpty(config["KeyVaultUri"]))
+                    {
+                        var secretClient = new SecretClient(new Uri(config["KeyVaultUri"]), new DefaultAzureCredential());
+                        builder.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+                    }
+                })
                 .UseStartup<Startup>();
     }
 }
