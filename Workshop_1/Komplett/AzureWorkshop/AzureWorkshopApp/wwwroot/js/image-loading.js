aa4fca7124bda2b5cddd5838009f1076be4287c6
Per-Frode Pedersen <per.pedersen@bouvet.no>
2023-01-02 14:53:28 +0100

    Updated WS1 - Komplett

--- Workshop_1/Komplett/AzureWorkshop/AzureWorkshopApp/wwwroot/js/image-loading.js
+++ Workshop_1/Komplett/AzureWorkshop/AzureWorkshopApp/wwwroot/js/image-loading.js
@@ -76,10 +76,10 @@ var handleError = function (response) { return __awaiter(_this, void 0, void 0, 
     return __generator(this, function (_b) {
         switch (_b.label) {
             case 0:
-                console.error(response);
+                console.log("Error", response);
                 errorContainer = document.getElementById("errors");
                 if (!(response.status !== 500)) return [3 /*break*/, 2];
-                return [4 /*yield*/, response.json()];
+                return [4 /*yield*/, response.text()];
             case 1:
                 _a = _b.sent();
                 return [3 /*break*/, 3];
@@ -89,7 +89,7 @@ var handleError = function (response) { return __awaiter(_this, void 0, void 0, 
             case 3:
                 error = _a;
                 paragraph = document.createElement("p");
-                paragraph.appendChild(document.createTextNode(error ? JSON.stringify(error) : response.statusText.toString()));
+                paragraph.appendChild(document.createTextNode(error ? error : response.statusText.toString()));
                 errorContainer.replaceChildren(paragraph);
                 ;
                 return [2 /*return*/];
